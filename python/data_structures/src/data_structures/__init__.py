3862ebce2e118b9b535e76d4313274cc261d3982
Cezar Todirisca <cezartodirisca@gmail.com>
2025-05-15 20:10:41 +0300

    Added code separation and basic config + Skip List
    - Added Base Classes for easier reuse of basic structures
    - Added Skip List data structure implementation in Python

---
+++ python/data_structures/src/data_structures/__init__.py
@@ -0,0 +1,14 @@
+"""A collection of data structures and algorithms implemented in Python.
+
+This package includes various data structures such as linked lists, trees, and graphs.
+"""
+
+from . import (
+    lib,
+)
+
+__all__ = [
+    "lib",
+    "lib.base",
+    "lib.skip_v1",
+]
